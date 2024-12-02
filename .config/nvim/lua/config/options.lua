911e9f6ee6782cc6b56b80bd511c192417bd0f54
bhuynhdev <bhuynhdev@gmail.com>
2024-12-01 21:40:01 -0500

    nvim: Make clipboard works in WSL

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -11,3 +11,20 @@ vim.opt.cindent = true
 vim.opt.cinkeys:remove("0#")
 vim.opt.cinkeys:remove("0#")
 vim.opt.indentkeys:remove("0#")
+
+-- Make clipboard works on WSL
+-- https://www.reddit.com/r/neovim/comments/10y3t48/im_struggling_to_get_clipboard_in_wsl_working/
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
