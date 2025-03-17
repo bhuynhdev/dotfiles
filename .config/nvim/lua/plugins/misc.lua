c4c0651143f05fde91586a3db6d0392db8849182
bhuynhdev <baoh.inbox@gmail.com>
2025-03-16 22:06:46 -0400

    nvim: Fix wrong braces level bug

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,14 +1,15 @@
 return {
   {
     "max397574/better-escape.nvim",
+    lazy = false,
     opts = {
       mapping = { "jk", "jj" },
     },
-    {
-      "andymass/vim-matchup",
-      setup = function()
-        vim.g.matchup_matchparen_offscreen = { method = "popup" }
-      end,
-    },
+  },
+  {
+    "andymass/vim-matchup",
+    setup = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
   },
 }
