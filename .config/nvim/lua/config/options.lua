ae4595bc6ac4ed7178fa457b1b8fa764722d61e6
bhuynhdev <baoh.inbox@gmail.com>
2025-03-16 22:06:47 -0400

    nvim: Use cindent and fix hash problem

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,3 +4,10 @@
 -- vim.opt.autochdir = true
 vim.opt.matchpairs:append("<:>")
 vim.opt.directory = "/tmp/vimswaps/" -- Shorter swap file dir to fix error https://github.com/neovim/neovim/issues/3898#issuecomment-372810680
+
+-- Restoring indent after typing hash: https://vim.fandom.com/wiki/Restoring_indent_after_typing_hash
+vim.opt.smartindent = false
+vim.opt.cindent = true
+vim.opt.cinkeys:remove("0#")
+vim.opt.cinkeys:remove("0#")
+vim.opt.indentkeys:remove("0#")
