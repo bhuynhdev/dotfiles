89fca98b1ee59bc63f8f7dc33b1bd9949af87164
bhuynhdev <baoh.inbox@gmail.com>
2025-03-16 22:06:47 -0400

    nvim: Add more file types for prettier

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -5,18 +5,28 @@ return {
     ---@param opts ConformOpts
     opts = function(_, opts)
       opts.formatters_by_ft = opts.formatters_by_ft or {}
-      local supported = { "javascript", "typescript", "jsx", "tsx" }
-      for _, ft in ipairs(supported) do
-        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
-        table.insert(opts.formatters_by_ft[ft], "prettierd")
+      local prettierd_supported = {
+        "svelte",
+        "css",
+        "graphql",
+        "handlebars",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        "less",
+        "markdown",
+        "markdown.mdx",
+        "scss",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "yaml",
+      }
+      for _, ft in ipairs(prettierd_supported) do
+        opts.formatters_by_ft[ft] = { "prettierd" }
       end
-
-      opts.formatters = opts.formatters or {}
-      -- opts.formatters.prettier = {
-      --   condition = function(_, ctx)
-      --     return M.has_parser(ctx) and (vim.g.lazyvim_prettier_needs_config ~= true or M.has_config(ctx))
-      --   end,
-      -- }
     end,
   },
 }
