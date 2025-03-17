5a051bc6c9f23d1d092e87152769860b30dbd0cf
bhuynhdev <baoh.inbox@gmail.com>
2025-03-16 22:06:46 -0400

    nvim: Change swap file directory
    Shorter swap file directory to fix error
    https://github.com/neovim/neovim/issues/3898#issuecomment-372810680

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,4 @@
 -- Add any additional options here
 -- vim.opt.autochdir = true
 vim.opt.matchpairs:append("<:>")
+vim.opt.directory = "/tmp/vimswaps/" -- Shorter swap file dir to fix error https://github.com/neovim/neovim/issues/3898#issuecomment-372810680
