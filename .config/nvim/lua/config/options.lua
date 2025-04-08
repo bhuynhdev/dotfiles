2f9e988eb44f8d33d5e80f5c57b78ae40debebc7
bhuynhdev <bao.huynh@online-rewards.com>
2025-04-08 10:19:32 -0400

    nvim: Disable autoformat

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -16,6 +16,8 @@ vim.g.snacks_animate = false
 
 vim.opt.expandtab = false
 
+vim.g.autoformat = false
+
 -- Make clipboard works on WSL
 -- https://www.reddit.com/r/neovim/comments/10y3t48/im_struggling_to_get_clipboard_in_wsl_working/
 if vim.fn.has("wsl") == 1 then
