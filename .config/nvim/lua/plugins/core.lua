e528a07ded3d0b8e85e427fc93975f38cdba4789
bhuynhdev <baoh.inbox@gmail.com>
2025-06-13 19:07:25 -0400

    Start LazyVim afresh

--- .config/nvim/lua/plugins/core.lua
+++
@@ -1,5 +0,0 @@
--- always use latest development version https://www.lazyvim.org/configuration/lazy.nvim
-return {
-  { "folke/lazy.nvim", version = false },
-  { "LazyVim/LazyVim", version = false },
-}
