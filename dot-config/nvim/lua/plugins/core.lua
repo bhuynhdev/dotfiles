befa9079ab62772c6f9b5db74a98d969a63b08be
bhuynhdev <baoh.inbox@gmail.com>
2025-06-13 19:31:02 -0400

    nvim: Always use latest neovim version

---
+++ dot-config/nvim/lua/plugins/core.lua
@@ -0,0 +1,5 @@
+-- always use latest development version https://www.lazyvim.org/configuration/lazy.nvimAdd commentMore actions
+return {
+  { "folke/lazy.nvim", version = false },
+  { "LazyVim/LazyVim", version = false },
+}
