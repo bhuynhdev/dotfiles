db2bf5a10702e8ceffc88aa2e13b7c2ec57a0a4d
bhuynhdev <bao.huynh@online-rewards.com>
2025-06-29 12:58:14 -0400

    nvim: Perl LSP to also consider current folder as lib source

---
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        perlnavigator = {
+          settings = {
+            perlnavigator = {
+              includePaths = vim.fn.glob(vim.fn.getcwd() .. "/*/lib", false, true),
+            },
+          },
+        },
+        lemminx = {
+          init_options = {
+            settings = {
+              -- All formatting options: https://github.com/redhat-developer/vscode-xml/blob/main/docs/Formatting.md
+              xml = {
+                format = {
+                  enabled = true,
+                  splitAttributes = "preserve",
+                  maxLineWidth = 280,
+                  preservedNewlines = 1,
+                },
+              },
+              xslt = {
+                format = {
+                  enabled = true,
+                  splitAttributes = "preserve",
+                  maxLineWidth = 280,
+                  preservedNewlines = 1,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
